f978ba5efcaf94010797323e577b3a5bbe319641
andreanmasiro <andreanmasiro@live.com>
2018-01-04 18:15:41 -0200

    added main storyboard, repository list scene sketch view controller

---
+++ desafio-concrete/desafio-concrete/Scenes/RepositoryListScene/RepositoryListViewController.swift
@@ -0,0 +1,23 @@
+//
+//  RepositoryListViewController.swift
+//  desafio-concrete
+//
+//  Created by André Marques da Silva Rodrigues on 04/01/18.
+//  Copyright © 2018 Vergil. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryListViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
