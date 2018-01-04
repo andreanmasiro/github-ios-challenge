f9ca27de3b2ac9c03b89a053ed36f9daa6578256
andreanmasiro <andreanmasiro@live.com>
2018-01-04 18:42:14 -0200

    added new cell repository cell

---
+++ desafio-concrete/desafio-concrete/Views/Cells/RepositoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RepositoryTableViewCell.swift
+//  desafio-concrete
+//
+//  Created by André Marques da Silva Rodrigues on 04/01/18.
+//  Copyright © 2018 Vergil. All rights reserved.
+//
+
+import UIKit
+
+class RepositoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
