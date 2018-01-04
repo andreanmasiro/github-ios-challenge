db7a84a6b68bdbca899a22c3c24e9f2abb57b95f
andreanmasiro <andreanmasiro@live.com>
2018-01-04 17:50:14 -0200

    added project

---
+++ desafio-concrete/desafio-concrete/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  desafio-concrete
+//
+//  Created by André Marques da Silva Rodrigues on 04/01/18.
+//  Copyright © 2018 Vergil. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+}
+
